f5f841fa1610039be5c92db7ce45253f8cbbd1eb
Morten Siebuhr <msi@one.com>
2013-04-11 11:54:51 +0200

    Add small test-suite (Mocha + Expect.js)

---
+++ test/basic.js
@@ -0,0 +1,23 @@
+var BB = require('../lib/BeanBag'),
+    expect = require('expect.js');
+
+describe('BeanBag({url: ..., illegalProperty: 1})', function () {
+    var bag;
+    before(function () {
+        bag = new BB({
+            url: 'http://localhost',
+            illegalProperty: 1
+        });
+    });
+
+    it('Shold be a BeanBag instance', function () {
+        expect(bag).to.be.an(BB);
+    });
+
+    it('Should not have illegalProperty set', function () {
+        expect(bag).not.to.have.property('illegalProperty');
+    });
+});
+
+
+
