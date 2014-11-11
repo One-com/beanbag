954592ac9506f348b1d143ae4569d57312e2fe56
Andreas Lind <andreas@one.com>
2014-11-11 08:33:31 +0100

    Don't silently discard options passed to the constructor. Extend 'this' or throw if it'd cause a conflict with an existing property.

--- test/BeanBag.js
+++ test/BeanBag.js
@@ -4,33 +4,25 @@ var BeanBag = require('../lib/BeanBag'),
     MockRequest = require('mockrequest');
 
 describe('BeanBag', function () {
-    describe('with an unsupported property passed to the constructor', function () {
-        var beanBag;
-        before(function () {
-            beanBag = new BeanBag({
+    it('should throw if a property passed to the constructor would conflict with something built-in', function () {
+        expect(function () {
+            new BeanBag({
                 url: 'http://localhost',
-                illegalProperty: 1
+                request: 1
             });
-        });
-
-        it('Should be a BeanBag instance', function () {
-            expect(beanBag, 'to be a', BeanBag);
-        });
-
-        it('Should not have illegalProperty set', function () {
-            expect(beanBag, 'not to have property', 'illegalProperty');
-        });
+        }, 'to throw', 'Unsupported config option: request (would conflict with built-in property)');
     });
 
     describe('with a url containing placeholders', function () {
         it('should substitute a placeholder with a value found in the options object passed to request', function (done) {
-            var beanBag = new BeanBag({url: 'http://{domainName}.contacts/foo/'});
-            beanBag.requestLibrary = new MockRequest({
-                request: {
-                    url: 'http://centersurf.net.contacts/foo/hey'
-                }
-            });
-            beanBag.request({
+            new BeanBag({
+                url: 'http://{domainName}.contacts/foo/',
+                requestLibrary: new MockRequest({
+                    request: {
+                        url: 'http://centersurf.net.contacts/foo/hey'
+                    }
+                })
+            }).request({
                 domainName: 'centersurf.net',
                 path: 'hey'
             }, done);
