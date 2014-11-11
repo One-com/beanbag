b406014cd7b4af811c42ec72ec03e0f0f82848e4
Andreas Lind <andreas@one.com>
2014-11-11 08:22:56 +0100

    request: Added support for substituting placeholders in the url.

--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,7 @@
 /*globals before, describe, it*/
 var BeanBag = require('../lib/BeanBag'),
-    expect = require('unexpected');
+    expect = require('unexpected'),
+    MockRequest = require('mockrequest');
 
 describe('BeanBag', function () {
     describe('with an unsupported property passed to the constructor', function () {
@@ -18,6 +19,21 @@ describe('BeanBag', function () {
 
         it('Should not have illegalProperty set', function () {
             expect(beanBag, 'not to have property', 'illegalProperty');
+        });
+    });
+
+    describe('with a url containing placeholders', function () {
+        it('should substitute a placeholder with a value found in the options object passed to request', function (done) {
+            var beanBag = new BeanBag({url: 'http://{domainName}.contacts/foo/'});
+            beanBag.requestLibrary = new MockRequest({
+                request: {
+                    url: 'http://centersurf.net.contacts/foo/hey'
+                }
+            });
+            beanBag.request({
+                domainName: 'centersurf.net',
+                path: 'hey'
+            }, done);
         });
     });
 });
