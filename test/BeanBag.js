330246bfbe1612018a916f9815f780b6ed509f5c
Andreas Lind <andreas@one.com>
2014-11-12 08:56:22 +0100

    Make sure to support placeholders in the url no matter how this.request is called.

--- test/BeanBag.js
+++ test/BeanBag.js
@@ -1,4 +1,4 @@
-/*globals before, describe, it*/
+/*globals describe, it, emit*/
 var BeanBag = require('../lib/BeanBag'),
     expect = require('unexpected'),
     MockRequest = require('mockrequest');
@@ -41,18 +41,6 @@ describe('BeanBag', function () {
             }).request({path: 'hey'}, done);
         });
 
-        it('should substitute a placeholder with a value found in the options object passed to the constructor', function (done) {
-            new BeanBag({
-                domainName: 'centersurf.net',
-                url: 'http://{domainName}.contacts/foo/',
-                requestLibrary: new MockRequest({
-                    request: {
-                        url: 'http://centersurf.net.contacts/foo/hey'
-                    }
-                })
-            }).request({path: 'hey'}, done);
-        });
-
         it('should substitute a placeholder with the result of calling a function of that name passed to the request method', function (done) {
             new BeanBag({
                 domainName: function (requestOptions, placeholderName) {
@@ -65,6 +53,70 @@ describe('BeanBag', function () {
                     }
                 })
             }).request({path: 'hey', owner: 'andreas@centersurf.net'}, done);
+        });
+
+        it('should substitute a placeholder with a value found in the options object passed to queryDesignDocument', function (done) {
+            new BeanBag({
+                designDocument: {
+                    views: {
+                        foo: {
+                            map: function () {
+                                emit('foo');
+                            }
+                        }
+                    }
+                },
+                url: 'http://{domainName}.contacts/foo/',
+                requestLibrary: new MockRequest({
+                    request: {
+                        url: 'http://centersurf.net.contacts/foo/_design/c5f85a319e5af7e66e88b89782890461/_view/foo'
+                    }
+                })
+            }).queryDesignDocument({
+                viewName: 'foo',
+                domainName: 'centersurf.net',
+                path: 'hey'
+            }, done);
+        });
+
+        it('should substitute a placeholder with a value found in the options object passed to queryDesignDocument when the design document does not exist yet', function (done) {
+            new BeanBag({
+                designDocument: {
+                    views: {
+                        foo: {
+                            map: function () {
+                                emit('foo');
+                            }
+                        }
+                    }
+                },
+                url: 'http://{domainName}.contacts/foo/',
+                requestLibrary: new MockRequest([
+                    {
+                        request: 'http://centersurf.net.contacts/foo/_design/c5f85a319e5af7e66e88b89782890461/_view/foo',
+                        response: 404
+                    },
+                    {
+                        request: 'PUT http://centersurf.net.contacts/foo/_design/c5f85a319e5af7e66e88b89782890461',
+                        response: 200
+                    },
+                    {
+                        request: 'http://centersurf.net.contacts/foo/_design/c5f85a319e5af7e66e88b89782890461/_view/foo'
+                    },
+                    {
+                        request: 'http://centersurf.net.contacts/foo/_all_docs?startkey=%22_design%2F%22&endkey=%22_design%2F~%22',
+                        response: {
+                            body: {
+                                rows: []
+                            }
+                        }
+                    }
+                ])
+            }).queryDesignDocument({
+                viewName: 'foo',
+                domainName: 'centersurf.net',
+                path: 'hey'
+            }, done);
         });
     });
 });
