4d6518a5d9a61465ab3d862ed200bac0880aa773
Andreas Lind <andreas@one.com>
2015-02-18 10:58:07 +0100

    Switch from mockrequest to unexpected-mitm.

--- test/BeanBag.js
+++ test/BeanBag.js
@@ -1,10 +1,24 @@
 /*globals describe, it, emit*/
 var BeanBag = require('../lib/BeanBag'),
-    expect = require('unexpected'),
+    unexpected = require('unexpected'),
     MockRequest = require('mockrequest'),
     passError = require('passerror');
 
 describe('BeanBag', function () {
+    var expect = unexpected.clone()
+        .installPlugin(require('unexpected-messy'))
+        .installPlugin(require('unexpected-mitm'))
+        .addAssertion('to call the callback with no error', function (expect, subject, done) {
+            subject(function (err) {
+                try {
+                    expect(err, 'to be falsy');
+                } catch (e) {
+                    return done(e);
+                }
+                done();
+            });
+        });
+
     it('should throw if a property passed to the constructor would conflict with something built-in', function () {
         expect(function () {
             new BeanBag({
@@ -16,36 +30,27 @@ describe('BeanBag', function () {
 
     describe('with a url containing placeholders', function () {
         it('should substitute a placeholder with a value found in the options object passed to request (and prefer it over an identically named one passed to the constructor)', function (done) {
-            new BeanBag({
+            var beanBag = new BeanBag({
                 domainName: 'the.wrong.one',
-                url: 'http://{domainName}.contacts/foo/',
-                requestLibrary: new MockRequest({
-                    request: {
-                        url: 'http://example.com.contacts/foo/hey'
-                    }
-                })
-            }).request({
-                domainName: 'example.com',
-                path: 'hey'
-            }, done);
+                url: 'http://{domainName}.contacts/foo/'
+            });
+
+            expect(function (cb) {
+                beanBag.request({
+                    domainName: 'example.com',
+                    path: 'hey'
+                }, cb);
+            }, 'with http mocked out', {
+                request: {
+                    url: 'http://example.com.contacts/foo/hey'
+                }
+            }, 'to call the callback with no error', done);
         });
 
         it('should substitute a complex expression in a placeholder', function (done) {
             var beanBag = new BeanBag({
                 url: 'http://couchdb{{partitionNumber} === 0 ? 3 : 4}.example.com/contacts{partitionNumber}',
-                partitionPoints: ['info'],
-                requestLibrary: new MockRequest([
-                    {
-                        request: {
-                            url: 'http://couchdb3.example.com/contacts0/hey'
-                        }
-                    },
-                    {
-                        request: {
-                            url: 'http://couchdb4.example.com/contacts1/there'
-                        }
-                    }
-                ])
+                partitionPoints: ['info']
             });
 
             beanBag.partitionNumber = function (requestOptions) {
@@ -57,68 +62,86 @@ describe('BeanBag', function () {
                     } else {
                         break;
                     }
-                }
+                    }
                 return databaseNumber;
             };
 
-            beanBag.request({
-                domainName: 'example.com',
-                path: 'hey'
-            }, passError(done, function () {
+            expect(function (cb) {
                 beanBag.request({
-                    domainName: 'example.info',
-                    path: 'there'
-                }, done);
-            }));
+                    domainName: 'example.com',
+                    path: 'hey'
+                }, passError(done, function () {
+                    beanBag.request({
+                        domainName: 'example.info',
+                        path: 'there'
+                    }, cb);
+                }));
+            }, 'with http mocked out', [
+                {
+                    request: {
+                        url: 'http://couchdb3.example.com/contacts0/hey'
+                    }
+                },
+                {
+                    request: {
+                        url: 'http://couchdb4.example.com/contacts1/there'
+                    }
+                }
+            ], 'to call the callback with no error', done);
         });
 
         it('should support passing a falsy value in request options', function (done) {
             var beanBag = new BeanBag({
                 url: 'http://couchdb{{partitionNumber} === 0 ? 3 : 4}.example.com/contacts{partitionNumber}',
-                partitionPoints: ['info'],
-                requestLibrary: new MockRequest([
-                    {
-                        request: {
-                            url: 'http://couchdb3.example.com/contacts0/hey'
-                        }
-                    }
-                ])
+                partitionPoints: ['info']
             });
-
-            beanBag.request({
-                partitionNumber: 0,
-                path: 'hey'
-            }, done);
+            expect(function (cb) {
+                beanBag.request({
+                    partitionNumber: 0,
+                    path: 'hey'
+                }, cb);
+            }, 'with http mocked out', [
+                {
+                    request: {
+                        url: 'http://couchdb3.example.com/contacts0/hey'
+                    }
+                }
+            ], 'to call the callback with no error', done);
         });
 
         it('should substitute a placeholder with a value found in the options object passed to the constructor', function (done) {
-            new BeanBag({
+            var beanBag = new BeanBag({
                 domainName: 'example.com',
-                url: 'http://{domainName}.contacts/foo/',
-                requestLibrary: new MockRequest({
-                    request: {
-                        url: 'http://example.com.contacts/foo/hey'
-                    }
-                })
-            }).request({path: 'hey'}, done);
+                url: 'http://{domainName}.contacts/foo/'
+            });
+            expect(function (cb) {
+                beanBag.request({path: 'hey'}, cb);
+            }, 'with http mocked out', {
+                request: {
+                    url: 'http://example.com.contacts/foo/hey'
+                }
+            }, 'to call the callback with no error', done);
+
         });
 
         it('should substitute a placeholder with the result of calling a function of that name passed to the request method', function (done) {
-            new BeanBag({
+            var beanBag = new BeanBag({
                 domainName: function (requestOptions, placeholderName) {
                     return requestOptions.owner.replace(/^.*@/, '');
                 },
-                url: 'http://{domainName}.contacts/foo/',
-                requestLibrary: new MockRequest({
-                    request: {
-                        url: 'http://example.com.contacts/foo/hey'
-                    }
-                })
-            }).request({path: 'hey', owner: 'andreas@example.com'}, done);
+                url: 'http://{domainName}.contacts/foo/'
+            });
+            expect(function (cb) {
+                beanBag.request({path: 'hey', owner: 'andreas@example.com'}, cb);
+            }, 'with http mocked out', {
+                request: {
+                    url: 'http://example.com.contacts/foo/hey'
+                }
+            }, 'to call the callback with no error', done);
         });
 
-        it('should substitute a placeholder with a value found in the options object passed to queryDesignDocument', function (done) {
-            new BeanBag({
+        describe('with a design document', function () {
+            var beanBag = new BeanBag({
                 designDocument: {
                     views: {
                         foo: {
@@ -128,32 +151,31 @@ describe('BeanBag', function () {
                         }
                     }
                 },
-                url: 'http://{domainName}.contacts/foo/',
-                requestLibrary: new MockRequest({
+                url: 'http://{domainName}.contacts/foo/'
+            });
+
+            it('should substitute a placeholder with a value found in the options object passed to queryDesignDocument', function (done) {
+                expect(function (cb) {
+                    beanBag.queryDesignDocument({
+                        viewName: 'foo',
+                        domainName: 'example.com',
+                        path: 'hey'
+                    }, cb);
+                }, 'with http mocked out', {
                     request: {
                         url: 'http://example.com.contacts/foo/_design/c5f85a319e5af7e66e88b89782890461/_view/foo'
                     }
-                })
-            }).queryDesignDocument({
-                viewName: 'foo',
-                domainName: 'example.com',
-                path: 'hey'
-            }, done);
-        });
+                }, 'to call the callback with no error', done);
+            });
 
-        it('should substitute a placeholder with a value found in the options object passed to queryDesignDocument when the design document does not exist yet', function (done) {
-            new BeanBag({
-                designDocument: {
-                    views: {
-                        foo: {
-                            map: function () {
-                                emit('foo');
-                            }
-                        }
-                    }
-                },
-                url: 'http://{domainName}.contacts/foo/',
-                requestLibrary: new MockRequest([
+            it('should substitute a placeholder with a value found in the options object passed to queryDesignDocument when the design document does not exist yet', function (done) {
+                expect(function (cb) {
+                    beanBag.queryDesignDocument({
+                        viewName: 'foo',
+                        domainName: 'example.com',
+                        path: 'hey'
+                    }, done);
+                }, 'with http mocked out', [
                     {
                         request: 'http://example.com.contacts/foo/_design/c5f85a319e5af7e66e88b89782890461/_view/foo',
                         response: 404
@@ -173,12 +195,8 @@ describe('BeanBag', function () {
                             }
                         }
                     }
-                ])
-            }).queryDesignDocument({
-                viewName: 'foo',
-                domainName: 'example.com',
-                path: 'hey'
-            }, done);
+                ], 'to call the callback with no error', done);
+            });
         });
     });
 });
