381db7e311ac544d3f92ef4f096472474c35f068
Andreas Lind <andreas@one.com>
2015-04-23 15:57:14 +0200

    Update unexpected to 7, and bump all other devDependencies.

--- test/BeanBag.js
+++ test/BeanBag.js
@@ -7,16 +7,14 @@ var BeanBag = require('../lib/BeanBag'),
 describe('BeanBag', function () {
     var expect = unexpected.clone()
         .installPlugin(require('unexpected-mitm'))
-        .addAssertion('to call the callback with no error', function (expect, subject, done) {
-            this.args.pop();
+        .addAssertion('to call the callback with no error', function (expect, subject) {
             this.errorMode = 'nested';
-            subject(function (err) {
-                try {
-                    expect(err, 'to be falsy');
-                } catch (e) {
-                    return done(e);
-                }
-                done();
+            return expect.promise(function (run) {
+                subject(run(function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                }));
             });
         });
 
@@ -27,17 +25,17 @@ describe('BeanBag', function () {
         }).request, 'to be a function');
     });
 
-    it('should perform a simple request', function (done) {
-        expect(function (cb) {
+    it('should perform a simple request', function () {
+        return expect(function (cb) {
             new BeanBag({ url: 'http://localhost:5984' }).request({ path: 'bar/quux' }, cb);
         }, 'with http mocked out', {
             request: 'GET http://localhost:5984/bar/quux',
             response: 200
-        }, 'to call the callback with no error', done);
+        }, 'to call the callback with no error');
     });
 
-    it('should allow specifying custom headers', function (done) {
-        expect(function (cb) {
+    it('should allow specifying custom headers', function () {
+        return expect(function (cb) {
             new BeanBag({ url: 'http://localhost:5984' }).request({ path: 'bar/quux', headers: { Foo: 'bar' } }, cb);
         }, 'with http mocked out', {
             request: {
@@ -45,22 +43,22 @@ describe('BeanBag', function () {
                 headers: { Foo: 'bar' }
             },
             response: 200
-        }, 'to call the callback with no error', done);
+        }, 'to call the callback with no error');
     });
 
-    it('should resolve the path from the base url', function (done) {
-        expect(function (cb) {
+    it('should resolve the path from the base url', function () {
+        return expect(function (cb) {
             new BeanBag({ url: 'http://localhost:5984/hey/there' }).request({ path: '../quux' }, cb);
         }, 'with http mocked out', {
             request: {
                 url: 'GET http://localhost:5984/hey/quux'
             },
             response: 200
-        }, 'to call the callback with no error', done);
+        }, 'to call the callback with no error');
     });
 
-    it('should allow specifying the request body as a Buffer', function (done) {
-        expect(function (cb) {
+    it('should allow specifying the request body as a Buffer', function () {
+        return expect(function (cb) {
             new BeanBag({ url: 'http://localhost:5984/' }).request({ method: 'POST', path: 'foo', body: new Buffer([1, 2, 3]) }, cb);
         }, 'with http mocked out', {
             request: {
@@ -71,11 +69,11 @@ describe('BeanBag', function () {
                 body: new Buffer([1, 2, 3])
             },
             response: 200
-        }, 'to call the callback with no error', done);
+        }, 'to call the callback with no error');
     });
 
-    it('should allow specifying the request body as a string', function (done) {
-        expect(function (cb) {
+    it('should allow specifying the request body as a string', function () {
+        return expect(function (cb) {
             new BeanBag({ url: 'http://localhost:5984/' }).request({ method: 'POST', path: 'foo', body: 'foobar' }, cb);
         }, 'with http mocked out', {
             request: {
@@ -83,14 +81,14 @@ describe('BeanBag', function () {
                 headers: {
                     'Content-Type': undefined
                 },
-                body: 'foobar'
+                body: new Buffer('foobar', 'utf-8')
             },
             response: 200
-        }, 'to call the callback with no error', done);
+        }, 'to call the callback with no error');
     });
 
-    it('should allow specifying the request body as an object, implying JSON', function (done) {
-        expect(function (cb) {
+    it('should allow specifying the request body as an object, implying JSON', function () {
+        return expect(function (cb) {
             new BeanBag({ url: 'http://localhost:5984/' }).request({ method: 'POST', path: 'foo', body: { what: 'gives' } }, cb);
         }, 'with http mocked out', {
             request: {
@@ -101,40 +99,40 @@ describe('BeanBag', function () {
                 body: { what: 'gives' }
             },
             response: 200
-        }, 'to call the callback with no error', done);
+        }, 'to call the callback with no error');
     });
 
-    it('should return an object with an abort method', function (done) {
-        expect(function (cb) {
+    it('should return an object with an abort method', function () {
+        return expect(function (cb) {
             expect(new BeanBag({ url: 'http://localhost:5984/' }).request({ method: 'POST', path: 'foo' }, cb), 'to satisfy', {
                 abort: expect.it('to be a function')
             });
         }, 'with http mocked out', {
             response: 200
-        }, 'to call the callback with no error', done);
+        }, 'to call the callback with no error');
     });
 
-    it.skip('should retry up to numRetries times', function (done) {
-        expect(function (cb) {
+    it.skip('should retry up to numRetries times', function () {
+        return expect(function (cb) {
             new BeanBag({ url: 'http://localhost:5984/' }).request({ path: 'foo', numRetries: 1 }, cb);
         }, 'with http mocked out', [
             { response: new Error('ETIMEDOUT') },
             { response: 200 }
-        ], 'to call the callback with no error', done);
+        ], 'to call the callback with no error');
     });
 
     describe('with a query', function () {
-        it('should allow specifying the query string as a string', function (done) {
-            expect(function (cb) {
+        it('should allow specifying the query string as a string', function () {
+            return expect(function (cb) {
                 new BeanBag({ url: 'http://localhost:5984/' }).request({ path: 'bar/quux', query: 'blabla' }, cb);
             }, 'with http mocked out', {
                 request: 'GET http://localhost:5984/bar/quux?blabla',
                 response: 200
-            }, 'to call the callback with no error', done);
+            }, 'to call the callback with no error');
         });
 
-        it('should allow specifying the query string as an object', function (done) {
-            expect(function (cb) {
+        it('should allow specifying the query string as an object', function () {
+            return expect(function (cb) {
                 new BeanBag({ url: 'http://localhost:5984/' }).request({ path: 'bar/quux', query: {
                     ascii: 'blabla',
                     nønascïî: 'nønascïî',
@@ -148,18 +146,18 @@ describe('BeanBag', function () {
                     '&multiple=%22foo%22' +
                     '&multiple=%22n%C3%B8nasc%C3%AF%C3%AE%22',
                 response: 200
-            }, 'to call the callback with no error', done);
+            }, 'to call the callback with no error');
         });
     });
 
     describe('with a url containing placeholders', function () {
-        it('should substitute a placeholder with a value found in the options object passed to request (and prefer it over an identically named one passed to the constructor)', function (done) {
+        it('should substitute a placeholder with a value found in the options object passed to request (and prefer it over an identically named one passed to the constructor)', function () {
             var beanBag = new BeanBag({
                 domainName: 'the.wrong.one',
                 url: 'http://{domainName}.contacts/foo/'
             });
 
-            expect(function (cb) {
+            return expect(function (cb) {
                 beanBag.request({
                     domainName: 'example.com',
                     path: 'hey'
@@ -168,10 +166,10 @@ describe('BeanBag', function () {
                 request: {
                     url: 'http://example.com.contacts/foo/hey'
                 }
-            }, 'to call the callback with no error', done);
+            }, 'to call the callback with no error');
         });
 
-        it('should substitute a complex expression in a placeholder', function (done) {
+        it('should substitute a complex expression in a placeholder', function () {
             var beanBag = new BeanBag({
                 url: 'http://couchdb{{partitionNumber} === 0 ? 3 : 4}.example.com/contacts{partitionNumber}',
                 partitionPoints: ['info']
@@ -190,16 +188,19 @@ describe('BeanBag', function () {
                 return databaseNumber;
             };
 
-            expect(function (cb) {
+            return expect(function (cb) {
                 beanBag.request({
                     domainName: 'example.com',
                     path: 'hey'
-                }, passError(done, function () {
+                }, function (err) {
+                    if (err) {
+                        throw err;
+                    }
                     beanBag.request({
                         domainName: 'example.info',
                         path: 'there'
                     }, cb);
-                }));
+                });
             }, 'with http mocked out', [
                 {
                     request: {
@@ -211,15 +212,15 @@ describe('BeanBag', function () {
                         url: 'http://couchdb4.example.com/contacts1/there'
                     }
                 }
-            ], 'to call the callback with no error', done);
+            ], 'to call the callback with no error');
         });
 
-        it('should support passing a falsy value in request options', function (done) {
+        it('should support passing a falsy value in request options', function () {
             var beanBag = new BeanBag({
                 url: 'http://couchdb{{partitionNumber} === 0 ? 3 : 4}.example.com/contacts{partitionNumber}',
                 partitionPoints: ['info']
             });
-            expect(function (cb) {
+            return expect(function (cb) {
                 beanBag.request({
                     partitionNumber: 0,
                     path: 'hey'
@@ -230,38 +231,39 @@ describe('BeanBag', function () {
                         url: 'http://couchdb3.example.com/contacts0/hey'
                     }
                 }
-            ], 'to call the callback with no error', done);
+            ], 'to call the callback with no error');
         });
 
-        it('should substitute a placeholder with a value found in the options object passed to the constructor', function (done) {
+        it('should substitute a placeholder with a value found in the options object passed to the constructor', function () {
             var beanBag = new BeanBag({
                 domainName: 'example.com',
                 url: 'http://{domainName}.contacts/foo/'
             });
-            expect(function (cb) {
+
+            return expect(function (cb) {
                 beanBag.request({path: 'hey'}, cb);
             }, 'with http mocked out', {
                 request: {
                     url: 'http://example.com.contacts/foo/hey'
                 }
-            }, 'to call the callback with no error', done);
-
+            }, 'to call the callback with no error');
         });
 
-        it('should substitute a placeholder with the result of calling a function of that name passed to the request method', function (done) {
+        it('should substitute a placeholder with the result of calling a function of that name passed to the request method', function () {
             var beanBag = new BeanBag({
                 domainName: function (requestOptions, placeholderName) {
                     return requestOptions.owner.replace(/^.*@/, '');
                 },
                 url: 'http://{domainName}.contacts/foo/'
             });
-            expect(function (cb) {
+
+            return expect(function (cb) {
                 beanBag.request({path: 'hey', owner: 'andreas@example.com'}, cb);
             }, 'with http mocked out', {
                 request: {
                     url: 'http://example.com.contacts/foo/hey'
                 }
-            }, 'to call the callback with no error', done);
+            }, 'to call the callback with no error');
         });
 
         describe('with a design document', function () {
@@ -278,8 +280,8 @@ describe('BeanBag', function () {
                 url: 'http://{domainName}.contacts/foo/'
             });
 
-            it('should substitute a placeholder with a value found in the options object passed to queryDesignDocument', function (done) {
-                expect(function (cb) {
+            it('should substitute a placeholder with a value found in the options object passed to queryDesignDocument', function () {
+                return expect(function (cb) {
                     beanBag.queryDesignDocument({
                         viewName: 'foo',
                         domainName: 'example.com',
@@ -289,11 +291,11 @@ describe('BeanBag', function () {
                     request: {
                         url: 'http://example.com.contacts/foo/_design/c5f85a319e5af7e66e88b89782890461/_view/foo'
                     }
-                }, 'to call the callback with no error', done);
+                }, 'to call the callback with no error');
             });
 
-            it('should substitute a placeholder with a value found in the options object passed to queryDesignDocument when the design document does not exist yet', function (done) {
-                expect(function (cb) {
+            it('should substitute a placeholder with a value found in the options object passed to queryDesignDocument when the design document does not exist yet', function () {
+                return expect(function (cb) {
                     beanBag.queryDesignDocument({
                         viewName: 'foo',
                         domainName: 'example.com',
@@ -320,7 +322,7 @@ describe('BeanBag', function () {
                     {
                         request: 'http://example.com.contacts/foo/_design/c5f85a319e5af7e66e88b89782890461/_view/foo'
                     }
-                ], 'to call the callback with no error', done);
+                ], 'to call the callback with no error');
             });
         });
     });
@@ -341,8 +343,8 @@ describe('BeanBag', function () {
                 });
             });
 
-            it('should make connections using the client certificate', function (done) {
-                expect(function (cb) {
+            it('should make connections using the client certificate', function () {
+                return expect(function (cb) {
                     beanBag.request({path: 'foo'}, cb);
                 }, 'with http mocked out', {
                     request: {
@@ -352,7 +354,7 @@ describe('BeanBag', function () {
                         key: one,
                         ca: two
                     }
-                }, 'to call the callback with no error', done);
+                }, 'to call the callback with no error');
             });
         });
 
@@ -375,8 +377,8 @@ describe('BeanBag', function () {
                 });
             });
 
-            it('should make connections using the client certificate', function (done) {
-                expect(function (cb) {
+            it('should make connections using the client certificate', function () {
+                return expect(function (cb) {
                     beanBag.request({path: 'foo'}, cb);
                 }, 'with http mocked out', {
                     request: {
@@ -386,7 +388,7 @@ describe('BeanBag', function () {
                         key: one,
                         ca: [two, three]
                     }
-                }, 'to call the callback with no error', done);
+                }, 'to call the callback with no error');
             });
         });
     });
